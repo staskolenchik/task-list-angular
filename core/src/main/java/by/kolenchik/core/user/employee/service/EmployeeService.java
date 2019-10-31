929a55e5277d7ac8a597615ae0579baa8fc1b158
Stas Kolenchik <stas.kolenchik@gmail.com>
2019-10-31 16:47:57 +0300

    read user from user service

--- core/src/main/java/by/kolenchik/core/user/employee/service/EmployeeService.java
+++ core/src/main/java/by/kolenchik/core/user/employee/service/EmployeeService.java
@@ -1,8 +1,13 @@
 package by.kolenchik.core.user.employee.service;
 
 import by.kolenchik.core.user.employee.Employee;
+import by.kolenchik.core.user.employee.dto.EmployeeInfoDto;
+
+import java.util.List;
 
 public interface EmployeeService {
 
     Employee add(Employee employee);
+
+    List<EmployeeInfoDto> findAll();
 }
