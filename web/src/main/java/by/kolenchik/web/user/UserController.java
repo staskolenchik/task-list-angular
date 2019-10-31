0a21b22f7e6da6ba3075aef4cecaf1f40e9f2f89
Stas Kolenchik <stas.kolenchik@gmail.com>
2019-10-31 16:48:36 +0300

    read user from user rest controller

---
+++ web/src/main/java/by/kolenchik/web/user/UserController.java
@@ -0,0 +1,25 @@
+package by.kolenchik.web.user;
+
+import by.kolenchik.core.user.employee.dto.EmployeeInfoDto;
+import by.kolenchik.core.user.employee.service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class UserController {
+
+    private EmployeeService employeeService;
+
+    public UserController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public List<EmployeeInfoDto> findAll() {
+        return employeeService.findAll();
+    }
+}
