163f444b7fc09df4b08fb036b022e498f3f55bac
Stas Kolenchik <stas.kolenchik@gmail.com>
2019-11-07 22:52:24 +0300

    configure angular material with pre-built theme

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {TaskService} from "./modules/tasks/task.service"
 
 @Component({
     selector: 'my-app',
@@ -17,7 +16,7 @@ import {TaskService} from "./modules/tasks/task.service"
             <router-outlet></router-outlet>
         </div>
     `,
-    providers: [TaskService]
+    styleUrls: ['./app.component.css'],
 })
 
 export class AppComponent {
