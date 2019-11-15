6940907f71597f5f99f2ec834d9b2e5b15221c40
Stas Kolenchik <stas.kolenchik@gmail.com>
2019-11-15 15:24:12 +0300

    change task form

---
+++ ui/src/app/modules/tasks/components/task-form/task-form.component.ts
@@ -0,0 +1,91 @@
+import {Component, EventEmitter, Input, Output} from "@angular/core";
+import {Task} from "../../../../shared/models/task";
+import {TaskType} from "../../../../shared/models/task-type";
+import {Employee} from "../../../../shared/models/employee";
+
+@Component({
+    selector: 'task-form-component',
+    template: `
+        <mat-card class="mat-elevation-z8">
+            <mat-card-title>Task Form</mat-card-title>
+            <mat-card-content>
+                <form class="task-form">
+                    <mat-form-field class="task-form__form-field">
+                        <input matInput
+                               placeholder="Subject"
+                               autofocus
+                               name="subject"
+                               [(ngModel)]="task.subject"/>
+                    </mat-form-field>
+
+                    <mat-form-field class="task-form__form-field">
+                        <textarea matInput
+                                  placeholder="Description"
+                                  name="description"
+                                  [(ngModel)]="task.description"></textarea>
+                    </mat-form-field>
+
+                    <label id="task-type-label" class="task-form__label-radio-button">Choose type</label>
+                    <mat-radio-group aria-labelledby="task-type-label"
+                                     class="task-form__radio-button-group"
+                                     name="taskType"
+                                     [(ngModel)]="task.type">
+                        <mat-radio-button *ngFor="let type of taskTypes"
+                                          class="task-form__radio-button"
+                                          [value]="type"
+                        >{{type}}</mat-radio-button>
+                    </mat-radio-group>
+
+                    <mat-form-field class="task-form__form-field">
+                        <mat-label>Assignee</mat-label>
+                        <mat-select name="assignee" [(ngModel)]="task.assigneeId">
+                            <mat-option *ngFor="let assignee of assignees" [value]="assignee.id">
+                                {{assignee.name}} {{assignee.surname}} {{assignee.patronymic}}
+                            </mat-option>
+                        </mat-select>
+                    </mat-form-field>
+                </form>
+            </mat-card-content>
+            <mat-card-actions>
+                <button mat-raised-button
+                        color="primary"
+                        (click)="onSubmit()"
+                >Save
+                </button>
+                <button mat-raised-button
+                        color="warn"
+                >Cancel
+                </button>
+            </mat-card-actions>
+        </mat-card>
+    `,
+    styles: [`
+        .task-form__form-field,
+        .task-form__label-radio-button,
+        .task-form__radio-button-group {
+            display: block;
+        }
+
+        .task-form__radio-button-group {
+            padding: 10px 0;
+        }
+        
+        .task-form__radio-button {
+            padding: 0 10px;
+        }
+    `]
+})
+
+export class TaskFormComponent {
+    private taskTypes: TaskType[] = [TaskType.STORY, TaskType.ISSUE];
+
+    @Input() private assignees: Employee[];
+    @Input() private task: Task;
+
+    @Output() add: EventEmitter<Task> = new EventEmitter();
+
+    onSubmit() {
+        this.task.createdById = 2;
+        this.add.emit(this.task);
+    }
+}
